d42207a4a35311053dce4bac5e07a0099ac58cc5
Angus Wallace <angus.wallace@flinders.edu.au>
2018-09-08 16:27:36 +0930

    add route planning

--- fire_app/app.R
+++ fire_app/app.R
@@ -10,6 +10,8 @@
 library(shiny)
 library(leaflet)
 library(ggmap)
+require(osrm)
+library(geosphere)
 
 #read in the location data
 lastRefugeDF<-read.table('/Users/wall0159/code/readyaimfire/fire_app/lastRefuge.csv')
@@ -43,8 +45,21 @@ server <- function(input, output) {
   points <- eventReactive(input$submit,{
 
     geocode(input$address, output = "latlon" , source = "google")
-
+    
   })
+  
+  route <- eventReactive(input$submit,{
+    # route planning
+    dists<-distm(as.vector(points()), lastRefugeDF, fun = distHaversine)
+    # sort them in acending distance, prune to first 10 sites
+    closestLastRefuges<-lastRefugeDF[order(dists),][1:3,]
+    # next find the route to each dist
+    osrmRoute(src = c("home", points()[1], points()[2]), 
+                       dst = c("last refuges", closestLastRefuges[1,1],closestLastRefuges[1,2]),
+                       sp = TRUE)
+  })
+  
+  
   
   output$mymap <- renderLeaflet({
     leaflet() %>%
@@ -57,7 +72,10 @@ server <- function(input, output) {
       # ) %>%
       addProviderTiles(providers$OpenTopoMap) %>%
       addMarkers(data = points()) %>%
+      addPolylines(data = route()) %>%
       addCircles(lng=lastRefugeDF$lng, lat=lastRefugeDF$lat, color='#00F')
+    
+      
   })
   
  
